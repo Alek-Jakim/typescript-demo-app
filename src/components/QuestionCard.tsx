8e6420950089e458d7d05885e79016947877779a
Alek <alek.jakimovski1@gmail.com>
2021-03-11 13:37:21 +0100

    boilerplate setup, api call func in API.ts

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+type Props = {
+    question: string;
+    answers: string[];
+    callback: any;
+    userAnswer: any;
+    questionNumber: number;
+    totalQuestions: number;
+}
+
+const QuestionCard: React.FC<Props> = ({ answers, callback, question, questionNumber, totalQuestions, userAnswer }) => {
+    return (
+        <div>
+            <p className="number">Question: {questionNumber}</p>
+            <p dangerouslySetInnerHTML={{ __html: question }}></p>
+            <div>
+                {answers.map(answer => (
+                    <div>
+                        <button disabled={userAnswer} onClick={callback}>
+                            <span dangerouslySetInnerHTML={{ __html: answer }}></span>
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default QuestionCard
